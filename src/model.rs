a308155f76cbba2b330d0dd07460a0a0eceabf75
Derek Mwale <barmwalederek@gmail.com>
2025-11-25 19:52:01 +0200

    segundo
    make it work

--- src/model.rs
+++ src/model.rs
@@ -1,27 +1,27 @@
-// ---------- main.rs (example usage) ----------
+// // ---------- main.rs (example usage) ----------
 
-#[tokio::main]
-async fn main() -> Result<(), ormigo::OrmigoError> {
-use ormigo::{OrmigoDB, QueryBuilder, User};
-
-
-let db = OrmigoDB::connect("sqlite://./example.db").await?;
+// #[tokio::main]
+// async fn main() -> Result<(), ormigo::OrmigoError> {
+// use ormigo::{OrmigoDB, QueryBuilder, User};
 
 
-let users = db
-.fetch_all::<User>(
-QueryBuilder::new("users")
-.filter("age > 21")
-.filter("is_active = 1")
-.select(&["id", "name", "email", "age", "is_active"]),
-)
-.await?;
+// let db = OrmigoDB::connect("sqlite://./example.db").await?;
 
 
-for user in users {
-println!("User: {:?}", user);
-}
+// let users = db
+// .fetch_all::<User>(
+// QueryBuilder::new("users")
+// .filter("age > 21")
+// .filter("is_active = 1")
+// .select(&["id", "name", "email", "age", "is_active"]),
+// )
+// .await?;
 
 
-Ok(())
-}
+// for user in users {
+// println!("User: {:?}", user);
+// }
+
+
+// Ok(())
+// }
